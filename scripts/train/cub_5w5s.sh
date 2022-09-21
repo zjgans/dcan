9d3f25db08120aa849a0f5221630f900a26a1105
zjgans <73008581+zjgans@users.noreply.github.com>
2022-09-21 22:12:13 +0800

    Update cub_5w5s.sh

--- scripts/train/cub_5w5s.sh
+++ scripts/train/cub_5w5s.sh
@@ -1,1 +1,1 @@
-python train.py -batch 64 -dataset cub -gpu 1 -extra_dir your_run -temperature_attn 5.0 -lamb 1 -shot 5 -milestones 60 70 -max_epoch 60
+python train.py -batch 64 -dataset cub -gpu 1 -extra_dir your_run -temperature_attn 5.0 -lamb 1 -shot 5 -milestones 60 70 -max_epoch 80
